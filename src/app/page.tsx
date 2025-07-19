7744bbefbc0cc21bb0dfaf06f3164306f97a8144
O'Brien <obrienadedapo15@gmail.com>
2025-07-20 00:39:03 +0100

    course section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <main className="relative overflow-hidden">
       {/* Hero Section */}
-      <div className="w-full flex flex-col md:flex-row items-center justify-cente gap-5">
+      <div className="w-full flex flex-col md:flex-row items-center gap-5">
         <div className="flex flex-col items-center gap-5">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl xl:text-7xl 2xl:text-8xl text-center italic text-primary300 2xl:pl-5">
+          <h1 className="min-[320px]:text-[24px] sm:text-3xl min-[380px]:text-[36px] max-[767px]:text-[36px] md:text-4xl xl:text-7xl 2xl:text-8xl text-center italic text-primary300 pt-5 md:pt-0 2xl:pl-5">
             Foydali ilmga tikilgan sarmoya,
             <span className="text-primary200">
               ikki dunyoda ham foyda beradi!
@@ -46,41 +46,97 @@ export default function Home() {
 
       {/* Category Section */}
       <div className="mt-7 px-5 md:px-10 lg:px-20">
-        <h2 className="text-primary100 font-bold text-4xl pb-4 mb-24 lg:mb-40 border-b-2 border-secondary400">
+        <h2 className="text-primary100 font-bold text-4xl lg:text-6xl 2xl:text-7xl pb-4 mb-40 lg:mb-40 border-b-2 border-secondary400">
           Kategoriya
         </h2>
+        <div className="relative">
+          <div className="absolute -inset-y-40 sm:-inset-y-28 md:-inset-y-40 lg:-inset-y-40 -right-20 sm:-right-20 md:-right-7 lg:-right-20">
+            <Image
+              alt="vector 1"
+              src={"/images/design-purple.png"}
+              width={180}
+              height={73}
+              priority
+            />
+          </div>
 
-        <div className="flex flex-col lg:flex-row px-2.5 gap-24 items-center justify-center mb-16 lg:mb-40">
-          <CategoryCard
-            image="/images/profile-picture.png"
-            title="Muhammadali Eshonqulov darslari"
-            users="2"
-            time="Umumiy 108
+          <div className="flex flex-col lg:flex-row px-2.5 gap-24 lg:gap-14 items-center justify-center mb-16 lg:mb-40">
+            <CategoryCard
+              image="/images/profile-picture.png"
+              title="Muhammadali Eshonqulov darslari"
+              users="2"
+              time="Umumiy 108
           "
-          />
+            />
 
-          <CategoryCard
-            image="/images/biznes.png"
-            title="Biznes Kvadrat darslari"
-            users="3"
-            time="Umumiy 320 
+            <CategoryCard
+              image="/images/biznes.png"
+              title="Biznes Kvadrat darslari"
+              users="3"
+              time="Umumiy 320 
           "
-          />
+            />
 
-          <CategoryCard
-            image="/images/biznes2.png"
-            title="Biznes Kvadrat ayollar"
-            users="4"
-            time="Umumiy 172
-          "
-          />
+            <CategoryCard
+              image="/images/biznes2.png"
+              title="Biznes Kvadrat ayollar"
+              users="4"
+              time="Umumiy 172
+            "
+            />
+          </div>
+
+          <div className="absolute -bottom-32 sm:bottom-28 md:-bottom-30 lg:-bottom-40 -left-20 sm:-left-20 md:-left-7 lg:-left-20">
+            <Image
+              alt="vector 2"
+              src={"/images/design-orange.png"}
+              width={180}
+              height={73}
+              priority
+            />
+          </div>
         </div>
       </div>
 
       {/* Top Courses Section*/}
+      <div className="mt-30 px-5 md:px-10 lg:px-20">
+        <div className="flex items-end justify-between relative">
+          <div className="w-full flex flex-col">
+            <h2 className="text-primary100 font-bold text-4xl lg:text-6xl 2xl:text-7xl pb-4 ">
+              Top Kurslar
+            </h2>
+
+            <hr className="w-full h-[2px] bg-secondary400" />
+          </div>
+
+          <div className="absolute md:-right-20">
+            <Image
+              alt="vector 3"
+              src={"/images/greenEllipse.svg"}
+              width={70}
+              height={70}
+              priority
+              className="hidden lg:block"
+            />
+          </div>
+
+          <div className="absolute -right-5 md:-right-10">
+            <Image
+              alt="vector 3"
+              src={"/images/greenEllipse.svg"}
+              width={35}
+              height={35}
+              priority
+              className="lg:hidden"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Top Books Section*/}
       <div className="mt-7 px-5 md:px-10 lg:px-20">
-        <h2 className="text-primary100 font-bold text-4xl pb-4 mb-16 lg:mb-40 border-b-2 border-secondary400">
-          Top Kurslar
+        <h2 className="text-primary100 font-bold text-4xl lg:text-6xl 2xl:text-7xl pb-4 mb-40 lg:mb-40 border-b-2 border-primary200">
+          Top 100 kitoblar
         </h2>
       </div>
     </main>
--- src/components/category-cards/index.tsx
+++ src/components/category-cards/index.tsx
@@ -1,40 +1,9 @@
-// import Image from "next/image";
-// import { CategoryCardProps } from "./types";
-
-// const CategoryCard = ({ image, title, users, time }: CategoryCardProps) => {
-//   return (
-//     <div className="relative w-full max-w-96 rounded-xl shadow-md overflow-hidden bg-white border-t-2">
-//       {/* Content */}
-//       <div className="p-4 flex flex-col items-center">
-//         <p className="text-primary300 text-2xl text-center mb-3.5">{title}</p>
-//         <div className="flex flex-col items-start justify-between gap-2 text-sm text-gray-600">
-//           <div className="flex items-center gap-1">
-//             <Image
-//               src="/images/user-reading.svg"
-//               alt="users"
-//               width={18.4}
-//               height={20}
-//             />
-//             <p className="text-[17px] font-medium">{users} ta dars</p>
-//           </div>
-//           <div className="flex items-center gap-1">
-//             <Image src="/images/clock.svg" alt="time" width={17} height={17} />
-//             <p className="text-[17px] font-medium">{time} soat</p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CategoryCard;
-
 import Image from "next/image";
 import { CategoryCardProps } from "./types";
 
 const CategoryCard = ({ image, title, users, time }: CategoryCardProps) => {
   return (
-    <div className="relative w-full max-w-96 bg-white rounded-xl shadow-xl pt-12 flex flex-col items-center border-t-2">
+    <div className="relative w-full max-w-96 xl:max-w-[500px] 2xl:max-w-[650px] bg-white rounded-xl shadow-xl pt-12 flex flex-col items-center border-t-2">
       {/* Circular Image - Overlapping Top */}
       <div className="absolute -top-28">
         <div className="rounded-full overflow-hidden shadow-md">
@@ -51,7 +20,7 @@ const CategoryCard = ({ image, title, users, time }: CategoryCardProps) => {
 
       {/* Content */}
       <div className="px-4 my-8 flex flex-col items-center">
-        <p className="text-primary300 h-16 text-2xl text-center mb-3.5">
+        <p className="min-[320px]:text-[18px] min-[375px]:text-[18px] min-[380px]:text-[24px] max-[767px]:text-[24px]  md:text-2xl lg:text-[17px] xl:text-2xl 2xl:text-[40px] text-primary300 h-16 xl:h-20 2xl:h-28 text-center mb-3.5">
           {title}
         </p>
         <div className="flex flex-col items-start justify-between gap-2 text-sm text-gray-600">
@@ -61,12 +30,37 @@ const CategoryCard = ({ image, title, users, time }: CategoryCardProps) => {
               alt="users"
               width={18.4}
               height={20}
+              className="2xl:hidden"
             />
-            <p className="text-[17px] font-medium">{users} ta dars</p>
+            <Image
+              src="/images/user-reading.svg"
+              alt="users"
+              width={38.8}
+              height={40}
+              className="hidden 2xl:block"
+            />
+            <p className="text-[17px] 2xl:text-[34px] font-medium">
+              {users} ta dars
+            </p>
           </div>
           <div className="flex items-center gap-1">
-            <Image src="/images/clock.svg" alt="time" width={17} height={17} />
-            <p className="text-[17px] font-medium">{time} soat</p>
+            <Image
+              src="/images/clock.svg"
+              alt="time"
+              width={17}
+              height={17}
+              className="2xl:hidden"
+            />
+            <Image
+              src="/images/clock.svg"
+              alt="time"
+              width={34}
+              height={34}
+              className="hidden 2xl:block"
+            />
+            <p className="text-[17px] 2xl:text-[34px] font-medium">
+              {time} soat
+            </p>
           </div>
         </div>
       </div>
